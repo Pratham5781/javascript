358e0e87ee122fcafa8868316007470f7dca3849
Pratham5781 <135578658+Pratham5781@users.noreply.github.com>
2026-09-06 03:57:52 +0000

    variables

---
+++ basics.1/01_variables.js
@@ -0,0 +1,22 @@
+const accountId =5164987
+let accountEmail="prathey8978@gmail.com"
+var accountPassword="654987"
+accountCity="meerut"
+
+//accountID=2 // not allowed
+accountEmail="Prathamd@345gmail.com"
+accountPassword="9632587410"
+accountCity="hapur"
+let accountState;
+
+
+
+
+
+console.log(accountId);
+
+/*
+prefer not to use var bcz of issue in block scope or functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
