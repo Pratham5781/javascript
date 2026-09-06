84f06f660bdef949a8b0e153c09111fd3aa97528
Pratham5781 <135578658+Pratham5781@users.noreply.github.com>
2026-09-06 03:57:29 +0000

    datatypes

---
+++ basics.1/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+/*alert(3+3)*/ //we are using node js not browsers
+
+console.log(3+3); console.log(" Pratham")//code readability bad 
+
+let name="pratham"
+let age=95
+let isLoggedIn =true
+// number => 2 to power 53
+// bugint
+// string => ""
+// boolean =>true/false
+// null =>standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "Pratham");
+console.log(typeof null);
+console.log(typeof undefined);
